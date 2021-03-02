acc71beec2c4d33bd93e079d4d058488e188d1b4
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-02 19:11:13 +0000

    Added eqArrays.js to compare arrays to each other

---
+++ eqArrays.js
@@ -0,0 +1,13 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  let isEqual;
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length && isEqual !== false; i++) {
+      if (arrayOne[i] === arrayTwo[i]) {
+        isEqual = true;
+      } else {
+        isEqual = false;
+      }
+    }
+  }
+  return (isEqual);
+};

ab73f476922fe172df4abe1d9bd04232439e346e
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-03 21:19:02 +0000

    added countLetters.js which counts how many instances of the same letter appears in a string and returns an object with the information

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function (actual, expected) {
+  const addQuotation = function (input) {
+    if (typeof (input) === "string") {
+      return ("\"" + input + "\"");
+    } else {
+      return (input);
+    }
+  };
+  actual = addQuotation(actual);
+  expected = addQuotation(expected);
+  (actual === expected) ? console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`) : console.log(`🔥🔥🔥Assertion Failed: ${actual} !== ${expected}`);
+};
+
+/* New Code */
+
+const countLetters = function (string) {
+  const stringArr = (string.replace(/\s+/g, '')).split('');
+  const newObject = {};
+
+  const count = function (letterArr, letter) {
+    let count = 0;
+    for (let item of letterArr) {
+      if (item === letter) {
+        count += 1;
+      }
+    }
+    return (count);
+  };
+
+  for (let letter of stringArr) {
+    newObject[letter] = count(stringArr, letter);
+  }
+  return (newObject);
+};
+
+console.log(countLetters("lighthouse in the house"));

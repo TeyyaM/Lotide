a6b0be3e274c19e8c20500879047f34e52f62a5b
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-08 21:48:46 +0000

    removed extraneous code at the top

--- countLetters.js
+++ countLetters.js
@@ -1,18 +1,3 @@
-const assertEqual = (actual, expected) => {
-  const addQuotation = input => {
-    if (typeof (input) === "string") {
-      return ("\"" + input + "\"");
-    } else {
-      return (input);
-    }
-  };
-  actual = addQuotation(actual);
-  expected = addQuotation(expected);
-  (actual === expected) ? console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`) : console.log(`🔥🔥🔥Assertion Failed: ${actual} !== ${expected}`);
-};
-
-/* New Code */
-
 const countLetters = string => {
   const stringArr = (string.replace(/\s+/g, '')).split('');
   const newObject = {};

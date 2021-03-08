d435aa68caf7fe6c1053e60ac91dfc3e896b8490
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-08 01:34:14 +0000

    updated eqArrays.js to handle nested arrays and objects within the arrays. Had to copy-paste eqArrays into eqObjects.js to avoid circular dependencies with require and module.exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,28 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let isEqual;
+const eqObjects = require('./eqObjects');
+
+const eqArrays = (arrayOne, arrayTwo) => {
   if (arrayOne.length === arrayTwo.length) {
-    for (let i = 0; i < arrayOne.length && isEqual !== false; i++) {
-      if (arrayOne[i] === arrayTwo[i]) {
-        isEqual = true;
-      } else {
-        isEqual = false;
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+        // Do array stuff
+        if (!eqArrays(arrayOne[i], arrayTwo[i])) {
+          return (false);
+        }
+        // Do object stuff
+      } else if (Array.isArray(arrayOne[i]) === false && typeof arrayOne[i] === 'object' && arrayOne[i] !== null) {
+        if (!eqObjects(arrayOne[i], arrayTwo[i])) {
+          return (false);
+        }
+        // End of new stuff
+      } else if (arrayOne[i] !== arrayTwo[i]) {
+        return (false);
       }
     }
+  } else {
+    return (false);
   }
-  return (isEqual);
+  return (true);
+
 };
 
 module.exports = eqArrays;
--- eqObjects.js
+++ eqObjects.js
@@ -1,5 +1,27 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = (arrayOne, arrayTwo) => {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+        // Do array stuff
+        if (!eqArrays(arrayOne[i], arrayTwo[i])) {
+          return (false);
+        }
+        // Do object stuff
+      } else if (Array.isArray(arrayOne[i]) === false && typeof arrayOne[i] === 'object' && arrayOne[i] !== null) {
+        if (!eqObjects(arrayOne[i], arrayTwo[i])) {
+          return (false);
+        }
+        // End of new stuff
+      } else if (arrayOne[i] !== arrayTwo[i]) {
+        return (false);
+      }
+    }
+  } else {
+    return (false);
+  }
+  return (true);
 
+};
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = (object1, object2) => {
